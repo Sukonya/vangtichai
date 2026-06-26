ccc13aae2d8daf11a0b82724e5226bad29c25236
Sukonya <sukonya.suki@gmail.com>
2026-06-27 04:48:17 +0600

    Add constants.dart with all the sizing values.

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+const double kAmountFontSize = 22.0;
+const double kLabelFontSize = 16.0;
+const double kButtonFontSize = 20.0;
+const double kTableFontSize = 15.0;
+const double kTabletAmountFontSize = 30.0;
+const double kTabletLabelFontSize = 22.0;
+const double kTabletButtonFontSize = 26.0;
+const double kTabletTableFontSize = 20.0;
+
+const double kButtonPadding = 8.0;
+const double kScreenPadding = 12.0;
+const double kTableRowHeight = 32.0;
+const double kTabletTableRowHeight = 44.0;
+
+const List<int> kNotes = [500, 100, 50, 20, 10, 5, 2, 1];
